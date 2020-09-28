bdba83e938d7ec92bf675890552c9599bca8b41f
just-osaze <osaigbevo.osaz@gmail.com>
2020-09-28 01:31:07 +0100

    Completed module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<app-product-page></app-product-page>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
