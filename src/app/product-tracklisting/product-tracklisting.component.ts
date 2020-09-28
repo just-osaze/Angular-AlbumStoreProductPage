703a5bae3076044df473c56f9e9e6f8c76722268
just-osaze <osaigbevo.osaz@gmail.com>
2020-09-28 11:31:17 +0100

    Completed module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injectable, OnInit } from '@angular/core';
-import { subscribeOn } from 'rxjs/operator/subscribeOn';
+
 import { Album } from '../album';
 import { ProductService } from '../product.service';
 
@@ -9,17 +9,13 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-tracklisting.component.css']
 })
 
-@Injectable (
-
-)
-
 export class ProductTracklistingComponent implements OnInit {
   albumInfo:Album;
-  constructor() { }
+  
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1)
-    subscribe() (response =>{
+    this._productService.getAlbum(1).subscribe(response =>{
       return this.albumInfo = response;
     });
   }
